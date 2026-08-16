59a06d6c039962124ab2ee3413a51fb9d3a0e760
DaGoMi1 <dlekrja4523@gmail.com>
2026-08-17 03:30:22 +0900

    feat: repository 설정

---
+++ app/repositories/start_location.py
@@ -0,0 +1,12 @@
+from sqlalchemy.orm import Session
+from app.models.start_location import StartLocation  # DB 모델
+
+class StartLocationRepository:
+    def __init__(self, db: Session):
+        self.db = db
+
+    def get_all(self):
+        return self.db.query(StartLocation).all()
+
+    def get_by_id(self, start_location_id: int):
+        return self.db.query(StartLocation).filter(StartLocation.kakao_place_id == start_location_id).first()
